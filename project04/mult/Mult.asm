36d0b8a70735daaf4e9c48aca8abb5402098f4ce
Daniel Johnson <danjohns@mymail.mines.edu>
2012-02-06 21:33:17 -0700

    Completed Mult.asm, 42=7*6 FTW

--- project04/mult/Mult.asm
+++ project04/mult/Mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+@2
+M=0
+@0
+D=M
+@INFINITE_LOOP
+D;JLE
+@1
+D=M
+@INFINITE_LOOP
+D;JLE
+(LOOP)
+	@1
+	D=M
+	@2
+	M=D+M
+	@0
+	M=M-1
+	D=M
+	@LOOP
+	D;JGT
+(INFINITE_LOOP)
+	@INFINITE_LOOP
+	0;JMP
