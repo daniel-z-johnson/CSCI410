e8873518e04c1ab3b255f42aea76992233aa1ebc
Daniel Johnson <danjohns@mymail.mines.edu>
2012-02-21 19:48:49 -0700

    Added files for c and instructions and started writeing them and testing them some

--- project06/assembler.rb
+++ project06/assembler.rb
@@ -1,3 +1,6 @@
+require "a_lines"
+require "c_lines"
+
 #make sure there is a input fiel
 if ARGV.count != 1
 	puts "Error should have one argument"
@@ -15,7 +18,13 @@ asm = File.new(ARGV[0], "r")
 f2name = ARGV[0].gsub(/\.asm/,".hack")
 #puts f2name
 hack = File.new(f2name, "w")
-
+asm.each do |i|
+	puts i
+end
+a = Aline.new
+a.testHash('R0')
+a.testHash('R11')
+a.testHash('R14')
 
 
 
