490e29b79a7da5af0831a9d55f7f7f00303520db
Daniel Johnson <danjohns@mymail.mines.edu>
2012-02-29 21:56:18 -0700

    Can open .vm files if file or dir provided and open asm files for writing

--- project07/vmTranslater.rb
+++ project07/vmTranslater.rb
@@ -1,5 +1,11 @@
 puts "Should have only one argument, note more less. One is the number"  unless ARGV.count == 1
 
+def parser_stage(input)
+	in_file = File.new(input, "r")
+	output = input.gsub(/\.vm$/,".asm")
+	out_file = File.new(output, "w")
+end
+
 
 def vm_file?(file)
 	/.vm$/.match(file)
@@ -9,10 +15,21 @@ if File.directory?(ARGV[0])
 	Dir.foreach(ARGV[0]) do |file|
 		next unless vm_file?(file)
 		puts "VM file found #{file}"
+		parser_stage(ARGV[0] + file)
+
 	end
 elsif vm_file?(ARGV[0])
+	begin
+		in_file = File.new(ARGV[0], "r")
+		in_file.close
+	rescue
+		puts "File #{ARGV[0]} does not exits"
+		exit
+	end
+	parser_stage(ARGV[0])
 	puts "Found VM file #{ARGV[0]}"
 else
 	puts "Expecting a directory or a vm file, niuether were suplied"
 end
-		
+
+
