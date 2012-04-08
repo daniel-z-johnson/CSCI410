3c82e16668730a7474af997cdaf58f4c75ec8b25
Daniel Johnson <danjohns@mymail.mines.edu>
2012-04-07 21:11:29 -0600

    Made it so files are written in same foulder as jack files, format is now XxxTgen.xml

--- project10/tokenizer.rb
+++ project10/tokenizer.rb
@@ -14,17 +14,21 @@ unless File.directory?(ARGV[0])
 	end
 
 	infile = File.new(ARGV[0],"r")
-	out_name = File.basename(ARGV[0]).gsub /\.jack$/, "T.xml"
-	out_name = "tokinized_files/" + out_name
+	out_name = ARGV[0].gsub /.jack$/, "Tgen.xml"
 	outFile = File.new(out_name,"w")
-	for i in infile
-		outFile.write i
-	end
 	
 	infile.close
 	outFile.close
 end
 
-Dir.foreach(ARGV[0]) do |file|
-	p file
+if File.directory?(ARGV[0])
+	Dir.chdir(ARGV[0])
+	Dir.foreach(".") do |file|
+		p File.basename file
+		if file.match /.jack$/
+			out_name = file.gsub(/.jack$/, "Tgen.xml")
+			outFile = File.new(out_name, "w")
+			outFile.close();
+		end
+	end
 end
