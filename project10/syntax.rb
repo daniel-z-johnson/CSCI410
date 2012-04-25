270fa0fa3bab497a822dc9b5ee956eecb000c963
Daniel Johnson <danjohns@mymail.mines.edu>
2012-04-24 20:34:12 -0600

    Completed class var dec function

--- project10/syntax.rb
+++ project10/syntax.rb
@@ -41,31 +41,75 @@ class Syntax
 
 	def compile_class
 		@out_file.puts "<class>"
-		a = @tokens.shift
-		until a == '{'
-			tok = @t.token_type(a)
-			write_toke tok, a
-			a = @tokens.shift
+		@a = @tokens.shift
+		until @a == '{'
+			tok = @t.token_type(@a)
+			write_toke tok, @a
+			@a = @tokens.shift
 		end
-		tok = @t.token_type(a)
-		write_toke tok, a
-		while @tokens[0].match /static|field/
+		tok = @t.token_type(@a)
+		write_toke tok, @a
+		@a = @tokens.shift
+		while @a.match /static|field/
 			compile_class_var_dec
 		end
-		while @tokens[0].match /constructor|function|method/
+		while @a.match /constructor|function|method/
 			compile_sub
 		end
-		a = @tokens.shift
-		unless a == '{'
-			puts "error expected } but got #{a} instead"
+		unless @a == '{'
+			puts "error expected } but got #{@a} instead"
 		end
 		@out_file.puts "</class>"
+	end
+
+	def compile_class_var_dec
+		@out_file.puts "<classVarDec>"
+		until @a == ';'
+			tok = @t.token_type(@a)
+			write_toke tok, @a
+			@a = @tokens.shift
+		end
+		tok = @t.token_type(@a)
+		write_toke tok, @a
+		@a = @tokens.shift
+
+		@out_file.puts "</classVarDec>"
+	end
+
+	def compile_sub
+	end
+
+	def compile_param_list
 	end
 
 	def compile_var_dec
 	end
 
-	def compile_sub
+	def compile_statement
+	end
+
+	def compile_do
+	end
+
+	def compile_let
+	end
+
+	def compile_while
+	end
+
+	def compile_return
+	end
+
+	def compile_if
+	end
+
+	def compile_expression
+	end
+
+	def compile_term
+	end
+
+	def compile_expression_list
 	end
 
 	def write_toke(type, n)
